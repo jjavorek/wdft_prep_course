15ceff3932e624c1d97d0f3046889e94602e00f3
Justin Javorek <justinjavorek@gmail.com>
2017-06-13 23:03:03 -0400

    #2

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -19,3 +19,10 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+var content = '';
+
+for (var i = 0; i < 7; i++) {
+    content = content + '#';
+    console.log(content);
+}
